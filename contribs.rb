69031e7013e83ece82ad636d9b99236ff0f6ab54
Guillaume Claret <dev@clarus.me>
2015-02-03 13:27:46 +0100

    Better detection of errors

--- contribs.rb
+++ contribs.rb
@@ -1,12 +1,29 @@
-contribs = `opam search --short coq:contrib:`.split(/\s/).map do |contrib|
-  contrib.strip["coq:contrib:".size..-1]
+require 'open3'
+
+output, status = Open3.capture2e("opam search --short coq:contrib:")
+if status.to_i == 0 then
+  contribs = output.split(/\s/).map do |contrib|
+    contrib.strip["coq:contrib:".size..-1]
+  end
+else
+  contribs = []
+end
+
+if contribs.size == 0 then
+  puts "You should set up an OPAM environment and add the `repo-unstable`."
+  exit(1)
 end
 
 case ARGV[0]
 when "clone"
   username = ARGV[1]
-  for contrib in contribs do
-    system("git clone #{username}@scm.gforge.inria.fr:/git/coq-contribs/#{contrib}.git")
+  if username then
+    for contrib in contribs do
+      system("git clone #{username}@scm.gforge.inria.fr:/git/coq-contribs/#{contrib}.git")
+    end
+  else
+    puts "GForge username expected."
+    exit(1)
   end
 when "list"
   puts contribs
@@ -14,9 +31,6 @@ else
   puts "\e[1;34mUsage:\e[0m
 
     ruby contribs.rb command
-
-We must be in an environment where OPAM and the repo-unstable are activated, so
-that `opam search coq:contrib:` get the list of contribs.
 
 \e[1;34mCommands:\e[0m
 - clone username: clone the contribs with the GForge username
